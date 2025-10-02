907e7a65827edc405a4dad5e83986b31b78fe2b0
gibran36631-lang <gibran36631@gmail.com>
2025-10-02 16:36:37 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 // pages/_app.js
 import '../styles/globals.css';
 
+// Estilos de react-slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
